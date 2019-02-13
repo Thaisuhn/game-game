5f5877c2fa6369261c0ada66c148205fdf2930b0
Tyson G <tlgreen16@gmail.com>
2019-02-12 20:26:11 -0500

    finished game

--- public/js/game.js
+++ public/js/game.js
@@ -84,10 +84,35 @@ function create(){
 
 	scoreText = this.add.text(16,16,"score:0",{fontSize: "32px", fill: "#000"});
 
+	let bombs = this.physics.add.group();
+
+	this.physics.add.collider(bombs,platforms);
+	this.physics.add.collider(player,bombs,hitBomb, null,this);
+
+	function hitBomb (player,bomb){
+		this.physics.pause();
+		player.setTint(0xff0000);
+		player.anims.play("turn");
+
+		gameOver = true;
+	}
+
 	function collectStar (player, star){
 		star.disableBody(true,true);
 		score += 10;
 		scoreText.setText("Score: " + score);
+		if(stars.countActive(true) === 0){
+			stars.children.iterate(function(child){
+				child.enableBody(true, child.x, 0, true, true);
+			});
+
+			let x = (player.x < 400) ? Phaser.Math.Between(400,800) : Phaser.Math.Between(0,400);
+
+			let bomb = bombs.create(x, 16, "bomb");
+			bomb.setBounce(1);
+			bomb.setCollideWorldBounds(true);
+			bomb.setVelocity(Phaser.Math.Between(-200,200),20);
+		}
 	}
 
 }
